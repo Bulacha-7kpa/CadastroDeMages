a2c07387d2bc104960903a4c7d0d0f77aff0e886
Bulacha-7kpa <138390308+Bulacha-7kpa@users.noreply.github.com>
2026-02-05 15:10:35 -0300

    Commit inicial

---
+++ src/main/java/com/ph/mastering_springboot/MasteringSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ph.mastering_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringbootApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(MasteringSpringbootApplication.class, args);
+	}
+
+}
